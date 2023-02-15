de7f3d4c7c7082b2340a7cb70147e0f534b2f162
Dima Potapchuk <traiget@gmail.com>
2023-02-15 20:16:23 +0200

    Init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Category {
+    id: number;
+    name: string;
+}
+
+const App = () => {
+    const [categories, setCategories] = useState<Array<Category>>([]);
+    const listItems = categories.map((category) =>
+        <li>{category.name}</li>
+    );
+    useEffect(() => {
+        axios.get(`http://localhost:8083/api/categories`)
+            .then(res => {
+                setCategories(res.data);
+            })
+            .catch(error => {
+                console.log("error", error);
+            });
+    }, []);
+    return (
+        <>
+            <h1>Categories</h1>
+            <ul>{listItems}</ul>
+        </>
+    );
 }
 
 export default App;
