95d660e83574731752ddd8391fe7a0878a45abec
Dmytro Potapchuk <traiget@gmail.com>
2023-02-20 19:22:34 +0200

    Add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./components/home";
 import {Route, Routes} from "react-router-dom";
 import DefaultLayout from "./components/containers/default";
 import Login from "./components/login";
+import NotFoundPage from "./components/notFound";
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
             <Route path="/" element={<DefaultLayout/>}>
                 <Route index element={<Home/>}/>
                 <Route path="login" element={<Login/>}/>
+                <Route path="*" element={<NotFoundPage/>}/>
             </Route>
         </Routes>
     );
