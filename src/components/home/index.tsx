4681477dbc7dce8c0f383ad95e54553b785c1ee2
Dima Potapchuk <traiget@gmail.com>
2023-03-01 19:10:18 +0200

    Fix bugs main page

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -39,7 +39,7 @@ const Home = () => {
                                     <div
                                         className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
                                         <img
-                                            src={"http://localhost:8083/files/" + category.image}
+                                            src={"http://localhost:8083/files/600_" + category.image}
                                             alt={category.image}
                                             className="h-full w-full object-cover object-center"
                                         />
