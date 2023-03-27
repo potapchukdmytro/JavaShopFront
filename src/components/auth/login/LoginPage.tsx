fe33a652e0ed3041e42c3585dd2551793b6e7b88
Dima Potapchuk <traiget@gmail.com>
2023-03-27 19:21:28 +0300

    Login page add formik

--- src/components/auth/login/LoginPage.tsx
+++ src/components/auth/login/LoginPage.tsx
@@ -3,39 +3,40 @@ import {ChangeEvent, useState} from "react";
 import {Link, useNavigate} from "react-router-dom";
 import {APP_ENV} from "../../../env";
 import {ILogin} from "../types";
+import * as yup from "yup";
+import {useFormik} from "formik";
 
 const LoginePage = () => {
     const navigator = useNavigate();
 
-    const [model, setModel] = useState<ILogin>({
+    // const [model, setModel] = useState<ILogin>({
+    //     email: "",
+    //     password: "",
+    // });
+
+    const initValues: ILogin = {
         email: "",
-        password: "",
-    });
-
-
-    const onChangeHandler = (
-        e:
-            | ChangeEvent<HTMLInputElement>
-            | ChangeEvent<HTMLTextAreaElement>
-            | ChangeEvent<HTMLSelectElement>
-    ) => {
-        //console.log(e.target.name, e.target.value);
-        setModel({...model, [e.target.name]: e.target.value});
+        password: ""
     };
 
+    const loginSchema = yup.object({
+        email: yup.string().required("Поле не може бути порожнім").email("Не вірний формат email"),
+        password: yup.string().required("Поле не може бути порожнім"),
+    });
 
-    const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    // const onChangeHandler = (
+    //     e:
+    //         | ChangeEvent<HTMLInputElement>
+    //         | ChangeEvent<HTMLTextAreaElement>
+    //         | ChangeEvent<HTMLSelectElement>
+    // ) => {
+    //     //console.log(e.target.name, e.target.value);
+    //     setModel({...model, [e.target.name]: e.target.value});
+    // };
+
+    const onSubmitHandler = async (values: ILogin) => {
         try {
-            const data = await axios.post(
-                `${APP_ENV.REMOTE_HOST_NAME}account/login`,
-                model,
-                // {
-                //   headers: {
-                //     "Content-Type": "multipart/form-data",
-                //   },
-                // }
-            );
+            const data = await axios.post(`${APP_ENV.REMOTE_HOST_NAME}account/login`, values);
             console.log("Login user token", data);
             //navigator("/");
         } catch (error: any) {
@@ -43,13 +44,20 @@ const LoginePage = () => {
         }
     };
 
+    const formik = useFormik({
+        initialValues: initValues,
+        onSubmit: onSubmitHandler,
+        validationSchema: loginSchema
+    });
+
+    const {values, errors,touched, handleSubmit, handleChange, setFieldValue} = formik;
 
     return (
         <div className="mx-auto max-w-7xl px-8">
             <div className="p-8 rounded mx-auto max-w-xl">
                 <h1 className="font-medium text-3xl text-center">Вхід на сайт</h1>
 
-                <form onSubmit={onSubmitHandler}>
+                <form onSubmit={handleSubmit}>
                     <div className="mt-8 grid lg:grid-cols-1 gap-4">
                         <div>
                             <label
@@ -61,12 +69,17 @@ const LoginePage = () => {
                             <input
                                 type="text"
                                 name="email"
-                                onChange={onChangeHandler}
-                                value={model.email}
+                                onChange={handleChange}
+                                value={values.email}
                                 id="email"
                                 className="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full"
 
                             />
+                            {errors.email && touched.email && (
+                                <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                                    <span className="font-medium">{errors.email}</span>
+                                </p>
+                            )}
                         </div>
 
                         <div>
@@ -79,12 +92,17 @@ const LoginePage = () => {
                             <input
                                 type="password"
                                 name="password"
-                                onChange={onChangeHandler}
-                                value={model.password}
+                                onChange={handleChange}
+                                value={values.password}
                                 id="password"
                                 className="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700 w-full"
 
                             />
+                            {errors.password && touched.password && (
+                                <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                                    <span className="font-medium">{errors.password}</span>
+                                </p>
+                            )}
                         </div>
 
 
